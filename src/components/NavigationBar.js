0b1903e8872bbcc10d7a4387e23e6bd62ca39990
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-09 03:11:57 +0500

    Navigation component completed with responsiveness

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/NavigationBar.css'
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircleChevronDown} from '@fortawesome/free-solid-svg-icons';
 
 function NavigationBar()
 {
     return(
         <section className="navigation">
-            <Navbar bg="dark" expand="md" id="nav" fixed="top">
+            <Navbar bg="dark" expand="lg" id="nav" fixed="top">
                 <Container className="py-2 px-4 px-sm-0">
 
                     {/* Navigation Bar Title */}
@@ -18,18 +20,20 @@ function NavigationBar()
 
                     {/* Navigation Bar Collapse */}
                     <Navbar.Toggle aria-controls="basic-navbar">
-                        <div className="menu-btn"></div>
+                        <div className="menu-btn">
+                            <FontAwesomeIcon icon={faCircleChevronDown} size="lg" />
+                        </div>
                     </Navbar.Toggle>
 
                     {/* Navigation Bar Menu */}
                     <Navbar.Collapse id="basic-navbar">
-                        <Nav className="ms-auto mt-3 mt-md-0 text-center">
-                            <Nav.Link href="" className="mx-md-2 active">Home</Nav.Link>
-                            <Nav.Link href="" className="mx-md-2">About</Nav.Link>
-                            <Nav.Link href="" className="mx-md-2">Skills</Nav.Link>
-                            <Nav.Link href="" className="mx-md-2">Work</Nav.Link>
-                            <Nav.Link href="" className="mx-md-2">Services</Nav.Link>
-                            <Nav.Link href="" className="mx-md-2">Contact</Nav.Link>
+                        <Nav className="ms-auto mt-3 mt-lg-0 text-center">
+                            <Nav.Link href="" className="mx-lg-2 active">Home</Nav.Link>
+                            <Nav.Link href="" className="mx-lg-2">About</Nav.Link>
+                            <Nav.Link href="" className="mx-lg-2">Skills</Nav.Link>
+                            <Nav.Link href="" className="mx-lg-2">Work</Nav.Link>
+                            <Nav.Link href="" className="mx-lg-2">Services</Nav.Link>
+                            <Nav.Link href="" className="mx-lg-2">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
 
