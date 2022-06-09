673278d430d24e6f8be237d7bd486b694fdbb8b2
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-10 04:03:59 +0500

    Files removed

--- src/contexts/skillsContext.js
+++
@@ -1,6 +0,0 @@
-import {createContext} from 'react';
-import SkillsData from '../data/skillsData';
-
-const SkillsContext = createContext(SkillsData);
-
-export default SkillsContext;
--- src/data/skillsData.js
+++
@@ -1,8 +0,0 @@
-const SkillsData = [
-    {skillname: "HTML", skillValue: 95},
-    {skillname: "CSS", skillValue: 95},
-    {skillname: "Bootstrap", skillValue: 95},
-    {skillname: "JavaScript", skillValue: 95},
-]
-
-export default SkillsData;
