af811aebb6ffbc9b841abe1f3e4854af9855709c
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-10 00:35:46 +0500

    Created services component

---
+++ src/components/Services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Default.css';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+
+function Services()
+{
+    return(
+        <section className="services" id="services">
+            <Container className="py-5">
+
+                <Row className="py-3 section-head text-center">
+                    <Col>
+                        <h2>Service</h2>
+                        <h5>What I Provide?</h5>
+                    </Col>
+                </Row>
+
+                <Row className="py-4">
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+export default Services;
