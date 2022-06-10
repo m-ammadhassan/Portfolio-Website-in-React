85e4e225cccef9f96cb26708952d1187c3bd0a1f
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-11 03:05:49 +0500

    Created skills data

---
+++ src/data/skillsData.js
@@ -0,0 +1,9 @@
+let SkillsData = [
+    {skillName: "HTML", skillValue: 95},
+    {skillName: "CSS", skillValue: 95},
+    {skillName: "Bootstrap", skillValue: 95},
+    {skillName: "JavaScript", skillValue: 95},
+    {skillName: "ES6", skillValue: 95}
+];
+
+export default SkillsData;
