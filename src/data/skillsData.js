82bc961c31132523f43ef7ee3b69ad7b09658eb8
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-11 18:53:28 +0500

    Added and updated the values

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,9 +1,11 @@
 let SkillsData = [
     {skillName: "HTML", skillValue: 95},
-    {skillName: "CSS", skillValue: 95},
+    {skillName: "CSS", skillValue: 90},
     {skillName: "Bootstrap", skillValue: 95},
-    {skillName: "JavaScript", skillValue: 95},
-    {skillName: "ES6", skillValue: 95}
+    {skillName: "JavaScript", skillValue: 85},
+    {skillName: "ES6", skillValue: 85},
+    {skillName: "ReactJS", skillValue: 80},
+    {skillName: "WordPress", skillValue: 90}
 ];
 
 export default SkillsData;
