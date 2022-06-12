df4112aaa9fd51354a14477ad7f8260867af8ecc
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-13 04:42:51 +0500

    Created work component data

---
+++ src/data/workData.js
@@ -0,0 +1,26 @@
+import HAYTravels from '../assets/images/portfolio/hay-travels.png';
+import HostelsUnited from '../assets/images/portfolio/hostels-united.png';
+
+let WorkData = [
+    {
+        id: 1,
+        projectName: "HAY Travels",
+        projectDetails: "HAY Travels is a Pakistani bus company that offers premium class travelling experience. Its aim is to make your travelling experience joyful and comfortable. This website offers you to book your bus tickets at one tap, allows to pay the ticket cost online, helps you to search out the bus schedule easily, 24/7 customer support and many more features available for the ease of customer.",
+        projectTechnologies: ["HTML", "CSS", "JavaScript"],
+        imageURL: HAYTravels,
+        githubURL: "https://github.com/m-ammadhassan/bus-travelling-website",
+        hostURL: "https://hay-travels.web.app/"
+    },
+
+    {
+        id: 2,
+        projectName: "Hostels United",
+        projectDetails: "We see many hotel booking companies but there is still a gap that How to accomodate a student or a job holder who travels to another city? To keep the all things in mind a service launched called Hostels United. It will help you to find out a valuable hostel according to your needs in a reasonable cost with all the details you want to know. You can also book a hostel with full payment or a payment in installments.",
+        projectTechnologies: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+        imageURL: HostelsUnited,
+        githubURL: "https://github.com/m-ammadhassan/Hostels-United",
+        hostURL: "https://hostels-united.web.app/"
+    }
+];
+
+export default WorkData;
