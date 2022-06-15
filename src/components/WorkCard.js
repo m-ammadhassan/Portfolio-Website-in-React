f7d22d7b8bc67d84fb983e79f825b261b498f473
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-15 17:36:46 +0500

    Completed work card component

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -9,18 +9,50 @@ import WorkModal from './WorkModal';
 
 function WorkCard()
 {
+    let initialModalData = [
+        {
+            id: 0,
+            projectName: "",
+            projectDetails: "",
+            projectTechnologies: ["dummySkill", "dummySkill"],
+            imageURL: "",
+            githubURL: "",
+            hostURL: ""
+        }
+    ]
+
     let [modal, setModal] = useState(false);
-    let [modalData, setModalData] = useState(null);
+    let [modalData, setModalData] = useState(initialModalData);
 
     let workList = useContext(WorkContext);
 
+    function cardClick(workData)
+    {
+        setModal(true);
+        setModalData(workData);
+    }
+
+    console.log(modalData);
+
     return(
         <>
+        <WorkModal 
+            show={modal} 
+            hide={()=>{setModal(false)}} 
+            title={modalData.projectName} 
+            desc={modalData.projectDetails} 
+            tech={modalData.projectTechnologies} 
+            image={modalData.imageURL} 
+            github={modalData.githubURL} 
+            host={modalData.hostURL}
+        />
+
+
         {
             workList.map( (work, i) => {
                 return(
-                    <Col lg={3} md={4} sm={6} className="mb-4 work-card" key={work.id}>
-                        <Card className="p-0" onClick={()=>{setModal(true); setModalData(work);}}>
+                    <Col lg={3} md={4} sm={6} className="mb-4 work-card" key={i}>
+                        <Card className="p-0" onClick={()=>{cardClick(work);}}>
                             <span style={{backgroundImage: `url(${work.imageURL})`}} className="m-0"></span>
                             <div className="overlay">
                                 <h6>{work.projectName}</h6>
@@ -31,17 +63,6 @@ function WorkCard()
                 ); 
             } )
         }
-
-        <WorkModal 
-            show={modal} 
-            hide={()=>{setModal(false)}} 
-            title={modalData.projectName} 
-            desc={modalData.projectDetails}
-            tech={modalData.projectTechnologies}
-            image={modalData.imageURL}
-            github={modalData.githubURL}
-            host={modalData.hostURL}
-        />
 
         </>
     );
