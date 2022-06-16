e05a49dbd559f112fbba23b7236e9af3a0049c8c
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-17 01:35:55 +0500

    Created social data list

---
+++ src/data/socialData.js
@@ -0,0 +1,26 @@
+let SocialData = [
+
+    {
+        socialMediaID: 1,
+        socialMediaName: "Facebook",
+        socialMediaIcon: "",
+        socialMediaLink: "",
+    },
+
+    {
+        socialMediaID: 2,
+        socialMediaName: "Twitter",
+        socialMediaIcon: "",
+        socialMediaLink: "",
+    },
+
+    {
+        socialMediaID: 3,
+        socialMediaName: "Instagram",
+        socialMediaIcon: "",
+        socialMediaLink: "",
+    },
+
+]
+
+export default SocialData;
