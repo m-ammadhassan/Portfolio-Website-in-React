f2bc52cfe3369c26a6a5a784ff720b4be7a418c6
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-17 01:34:45 +0500

    Updated skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,18 +12,18 @@ function Skills()
         <section className="skills" id="skills">
             <Container className="py-5">
 
-                <Row className="py-3 section-head text-center">
+                <Row className="py-3 mt-5 section-head text-center">
                     <Col>
                         <h2>My Skills</h2>
                         <h5>What I Know?</h5>
                     </Col>
                 </Row>
 
-                <Row className="py-4">
+                <Row className="pt-4 pb-5">
                 {
                     skillsList.map( (skill, i) => {
                         return(
-                        <Col lg={6} className="mt-4 px-4" key={i}>
+                        <Col lg={6} className="mt-4 mt-lg-5 px-4" key={i}>
                             <div className="skill-data d-flex justify-content-between">
                                 <h3>{skill.skillName}</h3>
                                 <h3>{`${skill.skillValue}%`}</h3>
