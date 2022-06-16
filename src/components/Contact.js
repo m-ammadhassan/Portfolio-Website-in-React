1ef74a9b7e1920dccf8db154ec8bf9c7323c03ff
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-17 01:32:57 +0500

    Completed contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/Default.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import '../styles/Contact.css';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import ContactForm from './ContactForm';
+import ContactImage from '../assets/images/contact.png';
 
 function Contact()
 {
@@ -8,14 +11,21 @@ function Contact()
         <section className="contact" id="contact">
             <Container className="py-5">
 
-                <Row className="py-3 section-head text-center">
+                <Row className="py-3 mt-5 section-head text-center">
                     <Col>
                         <h2>Contact ME</h2>
                         <h5>Have Any Query?</h5>
                     </Col>
                 </Row>
 
-                <Row className="py-4">
+                <Row className="py-4 justify-content-between">
+                    <Col sm={11} md={9} lg={5} className="mx-auto mx-lg-0">
+                        <Image src={ContactImage} fluid />
+                    </Col>
+
+                    <Col lg={6}>
+                        <ContactForm />
+                    </Col>
                 </Row>
             </Container>
         </section>
