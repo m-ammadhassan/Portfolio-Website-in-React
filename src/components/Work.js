98069b7416a6270dee1302c62f30b0f4dd8bffe8
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-17 01:34:59 +0500

    Updated work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,7 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import '../styles/Default.css';
 import '../styles/Work.css';
-import { Card, Col, Container, Nav, Row, Tab } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import WorkCard from './WorkCard';
 
 
@@ -11,14 +11,14 @@ function Work()
         <section className="work" id="work">
             <Container className="py-5">
 
-                <Row className="py-3 section-head text-center">
+                <Row className="py-3 mt-5 section-head text-center">
                     <Col>
                         <h2>My Work</h2>
                         <h5>What I Have Done?</h5>
                     </Col>
                 </Row>
 
-                <Row className="pt-4 justify-content-center">
+                <Row className="pt-4 pb-4 justify-content-center">
 
                     <WorkCard />
 
