694cc33bd6b9e396e9fa8f60e21dd406383dd2df
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-06-18 09:36:59 +0500

    Updated useButtonColorRemover hook

--- src/custom_hooks/useButtonColorRemover.js
+++ src/custom_hooks/useButtonColorRemover.js
@@ -5,8 +5,10 @@ export function useButtonColorRemover()
     useEffect( ()=>{
 
         let submitButton = document.querySelector(".submit-btn");
-
+        let hireButton = document.querySelector(".hire-btn");
+        
         submitButton.classList.remove("btn-primary");
+        hireButton.classList.remove("btn-primary");
     
     } );
 }
